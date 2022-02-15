6fee1c1dc31f809abfd6266c2f16b726a3573e2b
tanavel <tanavel1118@gmail.com>
2022-02-16 00:53:44 +0900

    prd環境でdebugを有効化する

--- src/config/environments/prd.php
+++ src/config/environments/prd.php
@@ -15,3 +15,12 @@ use Roots\WPConfig\Config;
  */
 
 // Config::define('DISALLOW_INDEXING', true);
+
+// NOTE: デバッグのため一時的にデバッグができるようにする
+Config::define('SAVEQUERIES', true);
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+Config::define('SCRIPT_DEBUG', true);
+Config::define('DISALLOW_INDEXING', true);
